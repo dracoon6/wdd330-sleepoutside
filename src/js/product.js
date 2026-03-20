9dd2a061167550402721b483945d11dbc8d06afe
dracoon6 <86674590+dracoon6@users.noreply.github.com>
2026-03-19 21:36:39 -0600

    work

--- src/js/product.js
+++ src/js/product.js
@@ -1,21 +1,11 @@
-import { getParam } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
+
+loadHeaderFooter();
 
 const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
-function updateCartCount() {
-  const cart = JSON.parse(localStorage.getItem("so-cart")) || [];
-  const count = cart.length;
-
-  const cartCount = document.querySelector(".cart-count");
-  if (cartCount) {
-    cartCount.textContent = count;
-  }
-}
-
-updateCartCount();
