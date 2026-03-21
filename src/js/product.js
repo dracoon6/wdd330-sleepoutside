f511120b5b2d552f06a67af1cb3d586d16246011
dracoon6 <86674590+dracoon6@users.noreply.github.com>
2026-03-21 13:53:46 -0600

    Revert "work"

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,21 @@
-import { getParam, loadHeaderFooter } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-
-loadHeaderFooter();
 
 const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
+
+function updateCartCount() {
+  const cart = JSON.parse(localStorage.getItem("so-cart")) || [];
+  const count = cart.length;
+
+  const cartCount = document.querySelector(".cart-count");
+  if (cartCount) {
+    cartCount.textContent = count;
+  }
+}
+
+updateCartCount();
