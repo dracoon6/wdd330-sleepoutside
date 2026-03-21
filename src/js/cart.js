7592fa6ef8dddb2f2aa9cff86f64588fcad5f93b
roboraton <133822199+roboraton@users.noreply.github.com>
2026-03-21 12:36:16 -0600

    Refactor shopping cart functionality; implement ShoppingCart class and streamline cart item rendering

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,49 +1,22 @@
-import { getLocalStorage, setLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import ShoppingCart from "./ShoppingCart.mjs";
+import { qs, loadHeaderFooter, getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 loadHeaderFooter();
 
-function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const productList = document.querySelector(".product-list");
-  if (cartItems && cartItems.length > 0) {
-    const htmlItems = cartItems.map((item, index) => cartItemTemplate(item, index));
-    productList.innerHTML = htmlItems.join("");
-  } else {
-    productList.innerHTML = "<li>Your cart is empty.</li>";
-  }
-}
-
-function cartItemTemplate(item, index) {
-  const newItem = `<li class="cart-card divider">
-    <span class="cart-card__remove" data-id="${index}">X</span>
-    <a href="#" class="cart-card__image">
-      <img
-        src="${item.Image}"
-        alt="${item.Name}"
-      />
-    </a>
-    <a href="#">
-      <h2 class="card__name">${item.Name}</h2>
-    </a>
-    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-    <p class="cart-card__quantity">qty: 1</p>
-    <p class="cart-card__price">$${item.FinalPrice}</p>
-  </li>`;
-
-  return newItem;
-}
+const listElement = qs(".product-list");
+const cart = new ShoppingCart(listElement);
+cart.init();
 
 function removeFromCart(itemIndex) {
   const cartItems = getLocalStorage("so-cart") || [];
   cartItems.splice(itemIndex, 1);
   setLocalStorage("so-cart", cartItems);
-  renderCartContents();
+
+  cart.init();
 }
 
-document.querySelector(".product-list").addEventListener("click", (e) => {
+listElement.addEventListener("click", (e) => {
   if (e.target.classList.contains("cart-card__remove")) {
     removeFromCart(e.target.dataset.id);
   }
 });
-
-renderCartContents();
